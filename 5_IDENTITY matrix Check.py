f0890c29858679d6deb8e8e124c0ecfff6e72462
Muhammad Tauseeq <tauseeq@AbuzerRauf-Desktop-51.arbisoft.com>
2018-08-16 11:07:54 +0500

    solved

--- 5_IDENTITY matrix Check.py
+++ 5_IDENTITY matrix Check.py
@@ -1,16 +1,29 @@
 # By Ashwath from forums
-# Given a list of lists representing a n * n matrix as input, 
-# define a  procedure that returns True if the input is an identity matrix 
+# Given a list of lists representing a n * n matrix as input,
+# define a  procedure that returns True if the input is an identity matrix
 # and False otherwise.
 
-# An IDENTITY matrix is a square matrix in which all the elements 
-# on the principal/main diagonal are 1 and all the elements outside 
-# the principal diagonal are 0. 
-# (A square matrix is a matrix in which the number of rows 
+# An IDENTITY matrix is a square matrix in which all the elements
+# on the principal/main diagonal are 1 and all the elements outside
+# the principal diagonal are 0.
+# (A square matrix is a matrix in which the number of rows
 # is equal to the number of columns)
 
 def is_identity_matrix(matrix):
-    #Write your code here
+
+    # check if square or not
+    if len(matrix)!=len(matrix[0]):
+        return False
+
+    n=len(matrix)
+    for i in range(n):
+        for j in range(n):
+            if i==j and matrix[i][j]!=1:
+                return False
+            elif i!=j and matrix[i][j]!=0:
+                return False
+    return True
+
 
 
 # Test Cases:
@@ -48,9 +61,9 @@ matrix5 = [[1,0,0,0,0,0,0,0,0]]
 print is_identity_matrix(matrix5)
 #>>>False
 
-matrix6 = [[1,0,0,0],  
-           [0,1,0,2],  
-           [0,0,1,0],  
+matrix6 = [[1,0,0,0],
+           [0,1,0,2],
+           [0,0,1,0],
            [0,0,0,1]]
 
 print is_identity_matrix(matrix6)
@@ -60,6 +73,4 @@ matrix7 = [[1, -1, 1],
            [0, 1, 0],
            [0, 0, 1]]
 print is_identity_matrix(matrix7)
-#>>>False           
-
-           
+#>>>False
