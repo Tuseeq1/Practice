8df441e1d81e80ce6d06e68c1a69aa00bb0e394b
Muhammad Tauseeq <tauseeq@AbuzerRauf-Desktop-51.arbisoft.com>
2018-08-17 10:31:14 +0500

    Solved

--- 19_Most Consective Repetitions.py
+++ 19_Most Consective Repetitions.py
@@ -1,14 +1,35 @@
 # Question 8: Longest Repetition
 
-# Define a procedure, longest_repetition, that takes as input a 
-# list, and returns the element in the list that has the most 
-# consecutive repetitions. If there are multiple elements that 
-# have the same number of longest repetitions, the result should 
-# be the one that appears first. If the input list is empty, 
+# Define a procedure, longest_repetition, that takes as input a
+# list, and returns the element in the list that has the most
+# consecutive repetitions. If there are multiple elements that
+# have the same number of longest repetitions, the result should
+# be the one that appears first. If the input list is empty,
 # it should return None.
 
 def longest_repetition(li):
-    # Your code here        
+    if not li:
+        return None
+
+    element=''
+    repetitions=0
+
+    i=0
+    while i<len(li)-1:
+        cur_element=li[i]
+        cur_repetition=1
+
+        while li[i+1]==cur_element and i+1<len(li)-1:
+            cur_repetition+=1
+            i+=1
+
+        if cur_repetition>repetitions:
+            repetitions=cur_repetition
+            element=cur_element
+        i+=1
+
+    return element
+
 
 
 
@@ -26,4 +47,3 @@ print longest_repetition([1,2,3,4,5])
 
 print longest_repetition([])
 # None
-
