8d23cc300e55637173574764815a05b42f9bbe94
Muhammad Tauseeq <tauseeq@AbuzerRauf-Desktop-51.arbisoft.com>
2018-08-27 11:33:57 +0500

    solved with mod operator

--- 14_Rotate Letter Encription.py
+++ 14_Rotate Letter Encription.py
@@ -6,29 +6,27 @@
 # Note that n can be positive, negative or zero.
 
 def rotate(string, number):
-    newstring=''
+    newstring = ''
     for letter in string:
-        if 'a'<=letter<='z':
-            asciiletter=ord(letter)
-            newletter=asciiletter+number
+        if 'a' <= letter <= 'z':
+            asciiletter = ord(letter) - 96
+            newletter = (asciiletter + number) % 26 + 96
 
-            if newletter>122:
-                newletter=96+(newletter-122)
-            elif newletter<97:
-                newletter=122-(96-newletter)
-            newstring+=chr(newletter)
+            newstring += chr(newletter)
+
         else:
-            newstring+=letter
+            newstring += letter
     return newstring
 
-print rotate ('sarah', 13)
-#>>> 'fnenu'
-print rotate('fnenu',13)
-#>>> 'sarah'
-print rotate('dave',5)
-#>>>'ifaj'
-print rotate('ifaj',-5)
-#>>>'dave'
+
+print rotate('sarah', 13)
+# >>> 'fnenu'
+print rotate('fnenu', 13)
+# >>> 'sarah'
+print rotate('dave', 5)
+# >>>'ifaj'
+print rotate('ifaj', -5)
+# >>>'dave'
 print rotate(("zw pfli tfuv nfibj tfiivtkcp pfl jyflcu "
-                "sv rscv kf ivru kyzj"),-17)
-#>>> ???
+              "sv rscv kf ivru kyzj"), -17)
+# >>> ???
