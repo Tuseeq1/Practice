dd58fbdd12bd47fdc8ec94172711ea3ef8441a46
Muhammad Tauseeq <tauseeq@AbuzerRauf-Desktop-51.arbisoft.com>
2018-08-27 14:56:14 +0500

    changed as requested

--- 10_Download Time Calculator.py
+++ 10_Download Time Calculator.py
@@ -26,55 +26,54 @@
 
 
 def download_time(filesize, fileUnit, BandWidth, BandWiddthUnit):
+    bits = {'kb': 1024, 'kB': 8192, 'Mb': 1048576, 'MB': 8388608, 'Gb': 1073741824,
+            'GB': 8589934592, 'Tb': 1099511627776, 'TB': 8796093022208}
 
-    bits={'kb':1024,'kB':8192,'Mb':1048576,'MB':8388608,'Gb':1073741824,
-            'GB':8589934592,'Tb':1099511627776,'TB':8796093022208}
-
-    filesize_bits=filesize*bits[fileUnit]
-    bandwidth_bits=BandWidth*bits[BandWiddthUnit]
+    filesize_bits = filesize * bits[fileUnit]
+    bandwidth_bits = BandWidth * bits[BandWiddthUnit]
 
     # download time in seconds
-    download_time=filesize_bits/bandwidth_bits
+    download_time = filesize_bits / bandwidth_bits
 
-    #converting download_time into hour minute and seconds mark
-    hours=int(download_time/3600)
-    minutes=int((download_time%3600)/60)
-    seconds=((download_time%3600)%60)
+    # converting download_time into hour minute and seconds mark
+    hours, m = divmod(download_time, 3600)  # int(download_time/3600)
+    minutes,m1 = divmod(m, 60)  # int((download_time%3600)/60)
+    seconds = m % 60
 
-    time=''
+    time = ''
 
-    if hours==1:
-        time=time+str(hours)+" hour "
+    if hours == 1:
+        time = time + str(int(hours)) + " hour, "
     else:
-        time=time+str(hours)+" hours "
+        time = time + str(int(hours)) + " hours, "
 
-    if minutes==1:
-        time=time+str(minutes)+" minute "
+    if minutes == 1:
+        time = time + str(int(minutes)) + " minute, "
     else:
-        time=time+str(minutes)+" minutes "
+        time = time + str(int(minutes)) + " minutes, "
 
-    if seconds==1 or seconds==1.0:
-        time=time+str(int(seconds))+" second"
+    if seconds == 1:
+        time = time + str(seconds) + " second"
     else:
-        time=time+str(seconds)+" seconds"
+        time = time + str(seconds) + " seconds"
 
     return time
 
 
-print download_time(1024,'kB', 1, 'MB')
-#>>> 0 hours, 0 minutes, 1 second
+print download_time(1024, 'kB', 1, 'MB')
+# >>> 0 hours, 0 minutes, 1 second
 
-print download_time(1024,'kB', 1, 'Mb')
-#>>> 0 hours, 0 minutes, 8 seconds  # 8.0 seconds is also acceptable
+print download_time(1024, 'kB', 1, 'Mb')
+# >>> 0 hours, 0 minutes, 8 seconds  # 8.0 seconds is also acceptable
 
-print download_time(13,'GB', 5.6, 'MB')
-#>>> 0 hours, 39 minutes, 37.1428571429 seconds
+print download_time(13, 'GB', 5.6, 'MB')
+# >>> 0 hours, 39 minutes, 37.1428571429 seconds
 
-print download_time(13,'GB', 5.6, 'Mb')
-#>>> 5 hours, 16 minutes, 57.1428571429 seconds
+print download_time(13, 'GB', 5.6, 'Mb')
+# >>> 5 hours, 16 minutes, 57.1428571429 seconds
 
-print download_time(10,'MB', 2, 'kB')
-#>>> 1 hour, 25 minutes, 20 seconds  # 20.0 seconds is also acceptable
+print download_time(10, 'MB', 2, 'kB')
+# >>> 1 hour, 25 minutes, 20 seconds  # 20.0 seconds is also acceptable
 
-print download_time(10,'MB', 2, 'kb')
-#>>> 11 hours, 22 minutes, 40 seconds  # 40.0 seconds is also acceptable
+print download_time(10, 'MB', 2, 'kb')
+# >>> 11 hours, 22 minutes, 40 seconds  # 40.0 seconds is also acceptable
