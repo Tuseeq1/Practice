2a821ea3c409dd5b6a281aeb358ee3f922f59a67
Muhammad Tauseeq <tauseeq@AbuzerRauf-Desktop-51.arbisoft.com>
2018-08-27 11:42:20 +0500

    solved with mod operator

--- 13_Next nth Letter.py
+++ 13_Next nth Letter.py
@@ -1,26 +1,22 @@
 # Write a procedure, shift_n_letters which takes as its input a lowercase
 # letter, a-z, and an integer n, and returns the letter n steps in the
 # alphabet after it. Note that 'a' follows 'z', and that n can be positive,
-#negative or zero.
+# negative or zero.
 
 def shift_n_letters(letter, n):
     # converting into ascii code for easy addition
-    asciiletter=ord(letter)
-    newletter=asciiletter+n
-
-    if newletter>122:
-        newletter=96+(newletter-122)
-    elif newletter<97:
-        newletter=122-(96-newletter)
+    asciiletter = ord(letter) - 96
+    newletter = (asciiletter + n) % 26 + 96
 
     return chr(newletter)
 
+
 print shift_n_letters('s', 1)
-#>>> t
+# >>> t
 print shift_n_letters('s', 2)
-#>>> u
+# >>> u
 print shift_n_letters('s', 10)
-#>>> c
+# >>> c
 print shift_n_letters('s', -10)
-#>>> i
-print shift_n_letters('z',1)
+# >>> i
+print shift_n_letters('z', 1)
