e76c603d2005eb510da378bfc8f1c2bac72a2ad7
Muhammad Tauseeq <tauseeq@AbuzerRauf-Desktop-51.arbisoft.com>
2018-08-28 10:25:41 +0500

    solved

--- 4_Symmitric Matrix Check.py
+++ 4_Symmitric Matrix Check.py
@@ -4,32 +4,39 @@
 # boolean True if the list is symmetric and False if it is not.
 
 def symmetric(matrix):
-    # Your code here
+    if len(matrix) != len(matrix[0]):
+        return False
+
+    transpose = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    if matrix != transpose:
+        return False
+    return True
+
 
 print symmetric([[1, 2, 3],
-                [2, 3, 4],
-                [3, 4, 1]])
-#>>> True
+                 [2, 3, 4],
+                 [3, 4, 1]])
+# >>> True
 
 print symmetric([["cat", "dog", "fish"],
-                ["dog", "dog", "fish"],
-                ["fish", "fish", "cat"]])
-#>>> True
+                 ["dog", "dog", "fish"],
+                 ["fish", "fish", "cat"]])
+# >>> True
 
 print symmetric([["cat", "dog", "fish"],
-                ["dog", "dog", "dog"],
-                ["fish","fish","cat"]])
-#>>> False
+                 ["dog", "dog", "dog"],
+                 ["fish", "fish", "cat"]])
+# >>> False
 
 print symmetric([[1, 2],
-                [2, 1]])
-#>>> True
+                 [2, 1]])
+# >>> True
 
 print symmetric([[1, 2, 3, 4],
-                [2, 3, 4, 5],
-                [3, 4, 5, 6]])
-#>>> False
+                 [2, 3, 4, 5],
+                 [3, 4, 5, 6]])
+# >>> False
 
-print symmetric([[1,2,3],
-                 [2,3,1]])
-#>>> False
+print symmetric([[1, 2, 3],
+                 [2, 3, 1]])
+# >>> False
